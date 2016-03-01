c86ae3fe73e7f5fe98dcbf17e498ed3e382b746b
Christo <chris@ozcrowd.com>
2016-03-01 17:27:08 +1100

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Christo on 1/03/2016.
+//  Copyright © 2016 Christopher Moltzen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
