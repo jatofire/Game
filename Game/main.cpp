7cbc95276ef1ed8a8dfaf635a8ab2e52dab58e5f
Christo <chris@ozcrowd.com>
2016-03-01 19:00:41 +1100

    Initial Commit

--- Game/main.cpp
+++ Game/main.cpp
@@ -7,9 +7,148 @@
 //
 
 #include <iostream>
+#include "SDL2/SDL.h"
+#include "SDL2_image/SDL_image.h"
+
+SDL_Window *window;
+SDL_Surface *gScreenSurface;
+SDL_Renderer *gRenderer;
+SDL_Texture* gTexture = NULL;
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+    
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial",
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  800,
+                                  450,
+                                  SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Initialize PNG loading
+            int imgFlags = IMG_INIT_PNG;
+            if( !( IMG_Init( imgFlags ) & imgFlags ) )
+            {
+                printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Get window surface
+               // gScreenSurface = SDL_GetWindowSurface( window );
+            }
+        }
+    }
+    
+    
+    gRenderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+    
+    if( gRenderer == NULL )
+    {
+        printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Initialize renderer color
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    
+    }
+    
+    
+    return success;
+}
+
+
+SDL_Texture* loadTexture( std::string path )
+{
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+    
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else
+    {
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+        
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+    
+    return newTexture;
+}
+
+
+
+
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    bool quit = false;
+    SDL_Event e;
+    init();
+
+    
+    gTexture = loadTexture("avatar.png");
+    //While application is running
+    while( !quit )
+    {
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+            //User requests quit
+            if( e.type == SDL_QUIT )
+            {
+                quit = true;
+            }
+        }
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    
+        //Clear screen
+        SDL_RenderClear( gRenderer );
+        
+        //Render texture to screen
+        SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
+        
+        
+        
+        //Update screen
+        SDL_RenderPresent( gRenderer );
+    }
+    
+
+    SDL_DestroyWindow(window);
+    
+    // Clean up
+    SDL_Quit();
+    
     return 0;
 }
